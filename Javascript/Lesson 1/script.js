3ae0e59c97b2e799d73e2ee4bea4c6ff90535683
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2024-12-31 22:14:11 +0600

    activity done

---
+++ Javascript/Lesson 1/script.js
@@ -0,0 +1,6 @@
+var age = 18
+document.write("My age is: "+age+" years")
+
+document.getElementById("heading_1").innerHTML = "JAVASCRIPT"
+
+document.getElementById("paragraph_1").innerText = "HELLO WORLD!"
