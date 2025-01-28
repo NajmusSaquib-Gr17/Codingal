73f8842d07637d6b64d4408500c291de71c832b8
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-01-28 22:35:06 +0600

    activity done

--- Advance Javascript/Lesson 3/script.js
+++ Advance Javascript/Lesson 3/script.js
@@ -10,7 +10,7 @@ let array_ascending_order = [5, 2, 9, 1, 7];
     array_ascending_order.sort(function(a, b) {
         return a - b;
     });
-    document.getElementById("demo1"). innerHTML = array_ascending_order;
+    document.getElementById("demo1"). innerHTML = array_ascending_order + " //Ascending Order";
 }
 
 
@@ -24,7 +24,7 @@ function array_2(){
     array_descending_order.sort(function(a,b){
         return b - a;
     })
-    document.getElementById("demo2").innerHTML = array_descending_order;
+    document.getElementById("demo2").innerHTML = array_descending_order + " //Decending Order";
 }
 
 //Reverse Order 
@@ -34,5 +34,16 @@ function array_3(){
     let array_reverse = [5, 2, 9, 1, 7];
 
     array_reverse.reverse();
-    document.getElementById("demo3").innerHTML = array_reverse;
+    document.getElementById("demo3").innerHTML = array_reverse + " //Reverse Order";
+}
+
+function array_4(){
+
+    let array_map = [5, 2, 9, 1, 7];
+
+    doubled = array_map.map(function(a){
+        return a * 2;
+    })
+
+    document.getElementById("demo4").innerHTML = doubled + " //Doubled Number";
 }
