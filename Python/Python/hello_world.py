147270fd564c02c8f4c1f15f7b98e115a6f0831c
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-03-04 22:12:20 +0600

    done

---
+++ Python/Python/hello_world.py
@@ -0,0 +1,26 @@
+
+def indentation():
+
+    print("Hello World!")
+    print(45)
+    age = 18
+    print("My age is \n",18)
+    
+
+indentation()
+
+def sum():
+    a = 5
+    b = 6
+    c = a + b
+    print(c)
+
+sum()
+
+def subtraction():
+    a = 1000
+    b = 542
+    c = a - b
+    print(c)
+
+subtraction()
