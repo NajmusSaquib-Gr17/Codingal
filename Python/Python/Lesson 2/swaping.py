c040b6ec092417dee26548b767405c62bc7d2e7b
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-03-06 22:10:13 +0600

    Swapping

---
+++ Python/Python/Lesson 2/swaping.py
@@ -0,0 +1,12 @@
+#Swapping in Python
+
+x = int(input("Enter first number: "))
+
+y = int(input("Enter second number: "))
+
+#Swapping
+temp = x
+x = y
+y = temp
+
+print(f"Values are: x -> {x}, y-> {y}")
