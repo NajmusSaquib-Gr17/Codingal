25a668a46b559aa8f1138110a31a5c4388bec121
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-03-06 22:03:07 +0600

    done

---
+++ Python/Python/Lesson 2/operators.py
@@ -0,0 +1,24 @@
+#Operators in Python >>>
+
+x = 50
+y = 25
+
+print(f"Addition Operator = {x+y}")
+print(f"Subtraction Operator = {x-y}")
+print(f"Multiplication Operator= {x*y}")
+print(f"Division Operator = {x/y}")
+print(f"Floor Division Operator = {x//y}")
+print(f"Exponential Operator  = {x**y}")
+
+print(f"is X equal to Y {x==y}")
+print(f"is X not equal to Y {x!=y}")
+
+first_name = "Najmus"
+last_name = "Saquib"
+full_name = first_name + last_name
+
+print(f"full_name = {full_name}")
+print(f"Length of the String full_name {len(full_name)}")
+print(f"indexing of string [3] {full_name[3]}")
+print(f"indexing of string [-2] {full_name[-2]}")
+print(f"indexing of string [0:3] {full_name[0:3]}")
