23f385017cc0b481ede6fa9dcf3d9d766885c9c4
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-03-06 21:50:23 +0600

    done

---
+++ Python/Python/Lesson 2/data_types.py
@@ -0,0 +1,23 @@
+#Data Types in Python
+def dataTypes():
+    age = 15
+    name = "Saquib"
+    isStudent = True
+    weight = 52.9
+
+    print(f"Name: {name}")
+    print(f"Date Type of {name} is {type(name)}")
+
+    age = int(age)
+    print(f"Age: {age}")
+    print(f"Date Type of {age} is {type(age)}")
+
+    isStudent = bool(False)
+    print(f"Is He a Student: {isStudent}")
+
+    weight = int(weight)
+    print(f"Weight: {weight}")
+    print(f"Date Type of {weight} is {type(weight)}")
+
+dataTypes()
+
