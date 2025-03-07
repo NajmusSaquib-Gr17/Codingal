e9ee8b8732e9057e7fa52141413ff1ce13bbf070
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-03-07 14:53:28 +0600

    done

---
+++ After Class Project/Python/Python Introduction/swapping.py
@@ -0,0 +1,19 @@
+#Swapping of Three Numbers
+
+num_1 = int(input("Enter Number 1 :"))
+num_2 = int(input("Enter Number 2 :"))
+num_3 = int(input("Enter Number 3 :"))
+
+#Swapping 
+temp = num_1
+
+num_1 = num_2
+
+num_2 = num_3
+
+num_3 = temp
+
+
+
+print(f"The numbers are swapped {num_1} {num_2} {num_3}")
+
