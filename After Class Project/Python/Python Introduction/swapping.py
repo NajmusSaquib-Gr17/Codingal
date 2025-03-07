9c02ffac519f936015a6fbf073bfd02495ffb02a
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-03-07 14:53:44 +0600

    change

--- After Class Project/Python/Python Introduction/swapping.py
+++ After Class Project/Python/Python Introduction/swapping.py
@@ -6,14 +6,9 @@ num_3 = int(input("Enter Number 3 :"))
 
 #Swapping 
 temp = num_1
-
 num_1 = num_2
-
 num_2 = num_3
-
 num_3 = temp
-
-
 
 print(f"The numbers are swapped {num_1} {num_2} {num_3}")
 
