67603737bc0097265985cbeb25f0cdf178f553fa
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-03-11 22:21:05 +0600

    done

--- Python/Python Introduction/Lesson 3/date and time.py
+++ Python/Python Introduction/Lesson 3/date and time.py
@@ -6,8 +6,5 @@ print("Current time: ", current_time)
 print(current_time.date())
 print(current_time.isoweekday())
 print(current_time.ctime())
-print(current_time.day())
-print(current_time.hour())
-print(current_time.microsecond())
 
 #print(calendar.calendar(100))
