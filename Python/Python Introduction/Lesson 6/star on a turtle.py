dd7a59e9e6dc7173e5959ba297a1e52b3a974e3c
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-03-20 22:26:58 +0600

    done

---
+++ Python/Python Introduction/Lesson 6/star on a turtle.py
@@ -0,0 +1,92 @@
+import turtle
+
+s = turtle.Screen()
+s.bgcolor('black')
+
+
+star = turtle.Turtle()
+star.color('green')
+star.pensize('3')
+star.fillcolor('orange')
+
+star.forward(100)
+star.left(120)
+star.forward(100)
+
+star.left(120)
+star.forward(100)
+
+star.penup()
+star.right(150)
+star.forward(50)
+
+star.pendown()
+star.right(90)
+star.forward(100)
+
+star.right(120)
+star.forward(100)
+star.right(120)
+star.forward(100)
+#--              ---
+# star.forward(100)
+# star.right(120)
+# star.forward(100)
+
+# star.right(120)
+# star.forward(100)
+
+# star.penup()
+# star.left(150)
+# star.forward(50)
+
+# star.pendown()
+# star.left(90)
+# star.forward(100)
+
+# star.left(120)
+# star.forward(100)
+# star.left(120)
+# star.forward(100)
+#-- --
+# star.forward(100)
+# star.left(120)
+# star.forward(100)
+
+# star.left(120)
+# star.forward(100)
+
+# star.penup()
+# star.right(150)
+# star.forward(50)
+
+# star.pendown()
+# star.right(90)
+# star.forward(100)
+
+# star.right(120)
+# star.forward(100)
+# star.right(120)
+# star.forward(100)
+# #---
+# star.forward(100)
+# star.right(120)
+# star.forward(100)
+
+# star.right(120)
+# star.forward(100)
+
+# star.penup()
+# star.left(150)
+# star.forward(50)
+
+# star.pendown()
+# star.left(90)
+# star.forward(100)
+
+# star.left(120)
+# star.forward(100)
+# star.left(120)
+# star.forward(100)
+
+turtle.done()
---
+++ Python/Python Introduction/Lesson 6/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+star.forward(100)
+# star.right(120)
+# star.forward(100)
+
+# star.right(120)
+# star.forward(100)
+
+# star.penup()
+# star.left(150)
+# star.forward(50)
+
+# star.pendown()
+# star.left(90)
+# star.forward(100)
+
+# star.left(120)
+# star.forward(100)
+# star.left(120)
+# star.forward(100)
