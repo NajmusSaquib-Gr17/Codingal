6b28de67f5591cb28356c8a50232c08065c28e14
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-04-10 18:45:40 +0600

    done

---
+++ After Class Project/Python/Python Introduction/Tuple To List/tuple_list.py
@@ -0,0 +1,8 @@
+personalInfo = ('John', 'Doe', '18 years', '180 cm', '58kg', 'Math')
+print(personalInfo)
+print(type(personalInfo))
+
+conversion = list(personalInfo)
+
+print(f'Converted to List:\n', conversion)
+print(type(conversion))
