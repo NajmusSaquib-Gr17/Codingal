f38b43319b325fe79482aa7b48d5c3e8ea29bf94
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-04-20 12:04:48 +0600

    done

--- After Class Project/Python/Python Introduction/Expression Class/expression class.py
+++ After Class Project/Python/Python Introduction/Expression Class/expression class.py
@@ -5,7 +5,7 @@ class number:
         self.num3 = num3
    
     def sum(self): 
-        print(f'Sum of the 3 Numbers {self.num1} \n {self.num1 + self.num2 +self.num3}')        
+        print(f'Sum of the 3 Numbers is: {self.num1 + self.num2 +self.num3}')        
 
 Numbers =  number(    
     num1 = int(input('Enter num1: ')),
