e437199c2194620e4b8f15b984c548c0004487e0
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-04-24 21:59:29 +0600

    activity done

---
+++ Python/Specialization in Python/File Handeling/Lesson 3/file-handling-ops-2.py
@@ -0,0 +1,11 @@
+#Create File
+# new_file = open('Python/Specialization in Python/File Handeling/Lesson 3/new-file.txt', 'x')
+# new_file.close()
+
+#Check if file exists
+import os
+# if os.path.exists('Python\Specialization in Python\File Handeling\Lesson 3\_new-file.txt'):
+#     print('File exists')
+# else:
+#     print("File doesn't exists")
+os.rmdir("D:/Codingal/Python/Specialization in Python/File Handeling/test_folder")
---
+++ Python/Specialization in Python/File Handeling/Lesson 3/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+new_file = open('Python/Specialization in Python/File Handeling/Lesson 3/new-file.txt', 'x')
+# new_file.close()
