d6c8c547dfc97d78bace3dca90a7304436555957
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-04-29 22:13:21 +0600

    done

---
+++ Python/Tkinter in Python/tkinter.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+window = Tk()
+window.title('Sample Window')
+window.geometry('400x400')
+window.configure(bg='light blue')
+
+greeting = Label(text='Hello! These is my first Tkinter', fg='black', bg='white')
+button = Button(text='Click me', fg='black', bg='white')
+entry = Entry(fg='yellow', bg='blue', width=50)
+
+greeting.pack()
+button.pack()
+entry.pack()
+
+frame = Frame(master=window, relief='ridge', borderwidth=5)
+frame.pack()
+
+label = Label(master=frame, text='This is a frame')
+label.pack()
+
+text = Text(master=frame, fg='yellow', bg='blue')
+text.pack()
