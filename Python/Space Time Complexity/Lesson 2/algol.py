f2191a4ed902490cb5def4de76bcb73147e672c3
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-05-13 22:19:27 +0600

    done

---
+++ Python/Space Time Complexity/Lesson 2/algol.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration = 0
+    print("The number entered by user is",n)
+    iteration+=1
+    print("Total iterations done by the code is", iteration,"\n")
+
+printnumber(1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+
+
