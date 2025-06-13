fec880be2b49931ce6a3be0ffc941c3fa26654e0
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-06-13 21:32:59 +0600

    done

---
+++ After Class Project/Python/Random Password Generator/randompasswordgenerator.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = [
+        random.choice(string.ascii_lowercase),
+        random.choice(string.ascii_uppercase),
+        random.choice(string.digits),
+        random.choice(string.punctuation)
+    ]
+    password += random.choices(chars, k=length - 4)
+    random.shuffle(password)
+    return ''.join(password)
+
+
+print(generate_password(10000))
