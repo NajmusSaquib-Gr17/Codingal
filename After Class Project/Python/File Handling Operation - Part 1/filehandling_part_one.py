9d75595d05a1bfad9faf597bb28f0a0d356ddd58
NajmusSaquib-Gr17 <nsaquib07@gmail.com>
2025-06-13 22:04:27 +0600

    done

---
+++ After Class Project/Python/File Handling Operation - Part 1/filehandling_part_one.py
@@ -0,0 +1,19 @@
+f = open("Sample_File.txt", "r")
+
+print(f.read())                # Full content
+
+f.seek(0)
+print(f.read(10))              # First 10 chars
+
+f.seek(0)
+print(f.readline())            # First line
+
+f.seek(0)
+for i in range(4):             # First 4 lines
+    print(f.readline(), end='')
+
+f.seek(0)
+for line in f:                 # All lines
+    print(line, end='')
+
+f.close()
